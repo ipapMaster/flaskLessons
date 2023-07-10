02c26a02d52816439ffc9d2571b7aa528d60670c
man <a@b.ru>
2023-07-10 11:59:37 +0300

    Different Decorators

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
+# https://github.com/ipapMaster/flaskLessons
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello!'
+    return 'Адмирал!<br><a href="/slogan">Слоган</a>'
+
+
+@app.route('/slogan')
+def slogan():
+    return 'Ибо крепка, как смерть, любовь!<br><a href="/">Назад</a>'
+
+
+@app.route('/countdown')
+def countdown():
+    lst = [str(x) for x in range(10, 0, -1)]
+    lst.append('Start!!!')
+    return '<br>'.join(lst)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
