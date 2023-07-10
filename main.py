7c25d22ee2353791a2c65c17c6f6dc05571ab3cb
man <a@b.ru>
2023-07-10 14:48:57 +0300

    Parameters

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 def index():
     return 'Адмирал!<br><a href="/slogan">Слоган</a>'
 
+
 @app.route('/poster')
 def poster():
     return f"""<!DOCTYPE html>
@@ -16,12 +17,16 @@ def poster():
 <head>
     <meta charset="UTF-8">
     <title>Постер</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
 </head>
 <body>
-<h1>Постер к фильму</h1>
+<h1 class="red">Постер к фильму</h1>
 <img src="{url_for('static', filename='images/admiral.png')}"
 alt="Здесь должна был быть картинка, но не нашлась">
-<p>И крепка, как смерть, любовь!</p>
+<p class="red">И крепка, как смерть, любовь!</p>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </body>
 </html>"""
 
@@ -37,8 +42,66 @@ def countdown():
     lst.append('Start!!!')
     return '<br>'.join(lst)
 
+@app.route('/greeting/<username>')
+def greeting(username):
+    return f"""<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>{username}</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
+    </head>
+    <body>
+    <h1>Привет, {username}</h1>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    </body>
+    </html>"""
 
+
+@app.route('/nekrasov')
+def nekrasov():
+    return f"""<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Постер</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
+    </head>
+    <body>
+    <div class="container">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Главная</a></li>
+        <li class="breadcrumb-item"><a href="#">Библиотека</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Данные</li>
+      </ol>
+    </nav>
+    <div class="row">
+    <div class="col">
+    <h1 class="red">Крестьянские дети</h1>
+    </div>
+    </div>
+    <div class="row">
+    <div class="col">
+    <img height="429" width="417"  src="{url_for('static', filename='images/nekrasov.png')}"
+    alt="Здесь должна был быть картинка, но не нашлась">
+    </div>
+    <div class="col">
+    <div class="bg-secondary text-white">Однажды, в студеную зимнюю пору,</div>
+    <div class="bg-success text-white">Я из лесу вышел; был сильный мороз.</div>
+    <div class="bg-secondary text-white">Гляжу, поднимается медленно в гору.</div>
+    <div class="bg-warning text-dark">Лошадка, везущая хворосту воз.</div>
+    <div class="bg-danger text-white">Читать далее...</div>
+    </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    </body>
+    </html>"""
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='127.0.0.1', port=5000, debug=True)
