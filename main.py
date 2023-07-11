27cccd2cc766f1c1f9f046a6f3d3c9a96531cdf9
man <a@b.ru>
2023-07-11 14:33:24 +0300

    Jinja

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # https://github.com/ipapMaster/flaskLessons
 from flask import Flask, url_for, request, redirect
+from flask import render_template
+import json
 
 app = Flask(__name__)
 
@@ -7,8 +9,30 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return redirect('/load_photo')  # безусловный редирект
+    param = {}
+    param['username'] = 'Слушатель'
+    param['title'] = 'Работа с шаблонами'
+    return render_template('index.html', **param)
 
+
+@app.route('/odd_even')
+def odd_even():
+    return render_template('odd_even.html', number=3)
+
+
+@app.route('/news')
+def news():
+    with open("news.json", "rt", encoding="utf-8") as f:
+        news_list = json.loads(f.read())
+    return render_template('news.html',
+                           title='Новости',
+                           news=news_list)
+    # lst = ['ANN', 'TOM', 'BOB']
+    # return render_template('news.html', title="FOR", news=lst)
+
+@app.route('/vartest')
+def vartest():
+    return render_template('var_test.html', title='Переменные в HTML')
 
 @app.route('/slogan')
 def slogan():
@@ -26,6 +50,7 @@ def form_sample():
         print(request.form['sname'])
         return 'Форма отправлена'
 
+
 @app.route('/load_photo', methods=['GET', 'POST'])
 def load_photo():
     if request.method == 'GET':
@@ -39,9 +64,10 @@ def load_photo():
         </form>
         """
     elif request.method == 'POST':
-        f = request.files['file']
+        f = request.files['file']  # request.form.get('file')
         f.save('./static/images/loaded.png')
         return '<h1>Файл у Вас на сервере</h1>'
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
