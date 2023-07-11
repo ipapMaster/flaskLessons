dfdd9ad964f0b5182ebb6b11523ac6a882697a11
man <a@b.ru>
2023-07-11 11:40:06 +0300

    Uploading File

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # https://github.com/ipapMaster/flaskLessons
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, redirect
 
 app = Flask(__name__)
 
@@ -7,179 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Адмирал!<br><a href="/slogan">Слоган</a>'
-
-
-@app.route('/poster')
-def poster():
-    return f"""<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Постер</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
-</head>
-<body>
-<h1 class="red">Постер к фильму</h1>
-<img src="{url_for('static', filename='images/admiral.png')}"
-alt="Здесь должна был быть картинка, но не нашлась">
-<p class="red">И крепка, как смерть, любовь!</p>
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-</body>
-</html>"""
+    return redirect('/load_photo')  # безусловный редирект
 
 
 @app.route('/slogan')
 def slogan():
     return 'Ибо крепка, как смерть, любовь!<br><a href="/">Назад</a>'
 
-
-@app.route('/countdown')
-def countdown():
-    lst = [str(x) for x in range(10, 0, -1)]
-    lst.append('Start!!!')
-    return '<br>'.join(lst)
-
-
-@app.route('/greeting/<username>')
-def greeting(username):
-    return f"""<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>{username}</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
-    </head>
-    <body>
-    <h1>Привет, {username}</h1>
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-    </body>
-    </html>"""
-
-
-@app.route('/slideshow')  # карусель
-def slideshow():
-    return f"""<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Постер</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
-    </head>
-    <body>
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="{url_for('static', filename='images/mars1.jpg')}" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="{url_for('static', filename='images/mars2.jpg')}" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="{url_for('static', filename='images/mars3.jpg')}" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-    </body>
-    </html>"""
-
-
-@app.route('/nekrasov')
-def nekrasov():
-    return f"""<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Постер</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
-    </head>
-    <body>
-    <div class="container">
-    <nav aria-label="breadcrumb">
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="#">Главная</a></li>
-        <li class="breadcrumb-item"><a href="#">Библиотека</a></li>
-        <li class="breadcrumb-item active" aria-current="page">Данные</li>
-      </ol>
-    </nav>
-    <div class="row">
-    <div class="col">
-    <h1 class="red">Крестьянские дети</h1>
-    </div>
-    </div>
-    <div class="row">
-    <div class="col">
-    <img height="429" width="417"  src="{url_for('static', filename='images/nekrasov.png')}"
-    alt="Здесь должна был быть картинка, но не нашлась">
-    </div>
-    <div class="col">
-    <div class="bg-secondary text-white">Однажды, в студеную зимнюю пору,</div>
-    <div class="bg-success text-white">Я из лесу вышел; был сильный мороз.</div>
-    <div class="bg-secondary text-white">Гляжу, поднимается медленно в гору.</div>
-    <div class="bg-warning text-dark">Лошадка, везущая хворосту воз.</div>
-    <div class="bg-danger text-white">Читать далее...</div>
-    </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-    </body>
-    </html>"""
-
-
-@app.route('/variants/<int:var>')
-def variants(var):
-    if var == 1:
-        return f"""<!doctype html>
-            <html lang="en">
-              <head>
-                <meta charset="utf-8">
-                <link rel="stylesheet" 
-                href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                crossorigin="anonymous">
-                <title>Варианты выбора</title>
-              </head>
-        <body>
-        <dl>
-        <dt>Пан или пропал</dt>
-        <dd>А что, нельзя выжить, став паном?</dd>
-        </dl>
-        </body></html>"""
-    elif var == 2:
-        return f"""<!doctype html>
-            <html lang="en">
-              <head>
-                <meta charset="utf-8">
-                <link rel="stylesheet" 
-                href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                crossorigin="anonymous">
-                <title>Варианты выбора</title>
-              </head>
-              <dl>
-                <dt>Даже если Вас съели, у Вас есть два</dt>
-                <dd>А в рассказах Мюнхаузена есть другой способ.</dd>
-              </dl>
-        </body>
-        </html>"""
-    else:
-        return "Я не знаю, о чём Вы!"
 
 @app.route('/form_sample', methods=['GET', 'POST'])
 def form_sample():
@@ -192,7 +26,22 @@ def form_sample():
         print(request.form['sname'])
         return 'Форма отправлена'
 
-
+@app.route('/load_photo', methods=['GET', 'POST'])
+def load_photo():
+    if request.method == 'GET':
+        return f"""
+        <form class="login_form" method="post" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="photo">Приложите фото:</label>
+                <input type="file" class="from-control-file" id="photo" name="file">
+            </div><br>
+            <button type="submit" class="btn btn-primary">Отправить</button>
+        </form>
+        """
+    elif request.method == 'POST':
+        f = request.files['file']
+        f.save('./static/images/loaded.png')
+        return '<h1>Файл у Вас на сервере</h1>'
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
