6baca4081ab9e0e68aab96a036bafe020cdaaf5b
man <a@b.ru>
2023-07-12 17:06:29 +0300

    ORM Start (Colomn should write from capital letter)

--- data/users.py
+++ data/users.py
@@ -6,12 +6,12 @@ from .db_session import SqlAlchemyBase
 class User(SqlAlchemyBase):
     __tablename__ = 'users'
 
-    id = sqlalchemy.column(sqlalchemy.Integer, primary_key=True,
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True,
                            autoincrement=True)
-    name = sqlalchemy.column(sqlalchemy.String, nullable=True)
-    about = sqlalchemy.column(sqlalchemy.String, nullable=True)
-    email = sqlalchemy.column(sqlalchemy.String, index=True,
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True,
                               unique=True, nullable=True)
-    hashed_password = sqlalchemy.column(sqlalchemy.String, nullable=True)
-    create_date = sqlalchemy.column(sqlalchemy.DateTime,
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    create_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                     default=datetime.datetime.now())
